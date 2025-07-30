566febc2deac2e3be55bcd04fc26566883c81473
Apurv Singh <rkumarsingh003@gmail.com>
2025-07-30 23:16:48 +0530

    CC START 197D

---
+++ Independence_Day.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    int res=15-x;
+    if(res>0||res==0)
+    cout<<res<<endl;
+    else
+    cout<<"-1"<<endl;
+}
